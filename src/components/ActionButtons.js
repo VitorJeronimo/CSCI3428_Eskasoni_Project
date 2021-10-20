3a1a8596bc0c49bb183d4179740cb949e7f58fc7
MeunierEvan <92047329+MeunierEvan@users.noreply.github.com>
2021-10-20 12:58:30 -0300

    Update ActionButtons.js

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -1,8 +1,8 @@
 const ActionButtons = () => {
   return (
     <section className="ActionButtons">
-      <button>New character</button>
-      <button>New List</button>
+      <button className="btn">New character</button>
+      <button className="btn">New List</button>
     </section>
   );
 }
