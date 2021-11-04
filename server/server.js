a5614aeb389e1948014d23fdd18b6b61e86b1cf7
Gillyweed <gillomaniac@gmail.com>
2021-11-04 16:36:45 -0300

    Added socket.io to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use(cors());
 
-app.get('/test', (req, res) => {
-  res.send({ express: 'EXPRESS BACKEND SUCCESSFULLY CONNECTED TO REACT'});
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
 });
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+// app.get('/test', (req, res) => {
+//   res.send({ express: 'EXPRESS BACKEND SUCCESSFULLY CONNECTED TO REACT'});
+// });
