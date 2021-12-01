7d72d98b3c892cf8f2c25e6a52d6068268ae2f57
Vitor Jeronimo <vitor.bently@hotmail.com>
2021-12-01 02:21:57 -0400

    Initial layout for the voting page

--- client/src/components/Word.js
+++ client/src/components/Word.js
@@ -1,10 +1,14 @@
 const Word = ({word}) => {
     return (
         <div className="Word">
-            <label className="WordLabel">{word}</label>
-            <label className="PlayerLabel">username</label>
-            <button className="voteYes">Yes</button>
-            <button className="voteNo">No</button>
+            <div className="ContentLeft">
+                <label className="WordLabel">{word}</label>
+            </div>
+            <div className="ContentRight">
+                <label className="PlayerLabel">username</label>
+                <button className="BtnVote VoteYes">Yes</button>
+                <button className="BtnVote VoteNo">No</button>
+            </div>
         </div>
     );
 }
--- client/src/components/WordList.js
+++ client/src/components/WordList.js
@@ -3,12 +3,10 @@ import Word from "./Word";
 const WordList = () => {
     const answers = ["guess#1", "guess2", "g3", "long_guess_answer#12343455", 'g5', 'g6']
     return (
-        <section className="WordList">
-            <div className="WordContainer">
-                {answers.map((word) => (
-                <Word word={word}/>))}
-            </div>
-        </section>
+        <div className="WordList">
+            {answers.map((word) => (
+            <Word word={word}/>))}
+        </div>
     );
 }
 
