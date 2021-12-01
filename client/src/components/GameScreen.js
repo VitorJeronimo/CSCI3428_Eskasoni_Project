5d426185db40672693bec6d4b6d1563e3773a724
Vitor Jeronimo <vitor.bently@hotmail.com>
2021-12-01 12:54:07 -0400

    Initial setup for audio

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -16,6 +16,29 @@ const GameScreen = ({ socket }) => {
     const history = useHistory();
     const location = useLocation();
 
+    const letters = [
+        {character: "A'", audio: "./audio/A_long.mp3"}, 
+        {character: "A", audio: "./audio/A.mp3"},
+        {character: "E'", audio: "./audio/E_long.mp3"},
+        {character: "E", audio: "./audio/E.mp3"},
+        {character: "I'", audio: "./audio/I_long.mp3"},
+        {character: "I", audio: "./audio/I.mp3"},
+        {character: "J", audio: "./audio/J.mp3"},
+        {character: "K", audio: "./audio/K.mp3"},
+        {character: "L", audio: "./audio/L.mp3"},
+        {character: "M", audio: "./audio/M.mp3"},
+        {character: "N", audio: "./audio/N.mp3"},
+        {character: "O'", audio: "./audio/O_long.mp3"},
+        {character: "O", audio: "./audio/O.mp3"},
+        {character: "P", audio: "./audio/P.mp3"},
+        {character: "Q", audio: "./audio/Q.mp3"},
+        {character: "S", audio: "./audio/S.mp3"},
+        {character: "A", audio: "./audio/SCHWA.mp3"},
+        {character: "T", audio: "./audio/T.mp3"},
+        {character: "U'", audio: "./audio/U_long.mp3"},
+        {character: "U", audio: "./audio/U.mp3"},
+        {character: "W", audio: "./audio/W.mp3"},
+    ]
     //===== STATES ==================================================================================
     // Game states
     const [roomState, setRoomState] = useState({});
@@ -47,6 +70,8 @@ const GameScreen = ({ socket }) => {
         history.push("/");
         window.alert("The server could not access your username and room ID. Please, log in again.");
     });
+
+    console.log(__dirname);
     const audio = new Audio(currentLetter.audio);
 
     const start = () => {
