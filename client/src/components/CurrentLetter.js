ecc9198e31c60c252fd58fed9a83f4f2a05b92b5
Vitor Jeronimo <vitor.bently@hotmail.com>
2021-12-04 04:22:27 -0400

    Fixed InitialLetter style and added sound button

--- client/src/components/CurrentLetter.js
+++ client/src/components/CurrentLetter.js
@@ -3,7 +3,10 @@
 const CurrentLetter = ({currentLetter}) => {
   return (
     <section className="InitialLetter">
-      <h1>{ currentLetter.character }</h1>
+      <button className="Btn LetterSound">🔊</button>
+      <div className="LetterContainer">
+          <h1>{ currentLetter.character }</h1>
+      </div>
     </section>
   );
 }
