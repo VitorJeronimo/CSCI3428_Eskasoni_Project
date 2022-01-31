abc80af2212daf15a632d5df600e620fcbeb9007
Vitor Jeronimo <vitor.bently@hotmail.com>
2022-01-31 19:01:11 -0400

    Refactored "join_lobby" event

--- server/modules/lobby.js
+++ server/modules/lobby.js
@@ -1,19 +1,67 @@
 const rooms = new Map();
 const players = new Map();
 
+const defaultGameState = {
+    currentLetter: {},
+    currentLetter: [],
+    gameDuration: 120,
+    gameStarted: false
+}
+
 class Connection {
     constructor(io, socket) {
         this.io = io;
         this.socket = socket;
 
-        socket.on('join', ({ name, room }) => this.addPlayer(name, room));
+        socket.on('join_lobby', ({ name, room }) => this.addPlayer(name, room));
+        socket.on('start_game', () => this.startGame());
         socket.on('disconnect', () => this.disconnect());
     }
 
-    addPlayer(name, room) {
-        // TODO: implement "addPlayer" method
-        players.set(this.socket.id, {name: name, room: room});
-        console.log(players)
+    addPlayer(playerName, roomID) {
+        const newPlayer = {
+            name: playerName,
+            room: roomID,
+            isAdmin: false,
+            score: 0,
+            gameWords: []
+        };
+
+        if (rooms.has(roomID)) {
+            const room = rooms.get(roomID);
+            const playersList = room.playersList;
+            playersList.set(socket.id, newPlayer);
+        }
+        else {
+            const playersList = new Map();
+            newPlayer.isAdmin = true;
+            playersList.set(socket.id, newPlayer);
+
+            const newRoom = {
+                admin: newPlayer,
+                playersList: playersList,
+                gameState: defaultGameState
+            }
+
+            rooms.set(roomID, newRoom);
+            socket.emit('hide_buttons');
+        }
+        
+        players.set(socket.id, newPlayer)
+        socket.join(roomID);
+    }
+
+    startGame() {
+    // Get info of the player that emitted the event
+        const player = Player.getCurrentPlayer(socket.id);
+        const room = Room.getCurrentRoom(player.roomName);
+        
+        // Only allow the game to start if the player is the room admin
+        if (player === room.admin) {
+            room.updateRoom();
+            room.startGame();
+            io.to(room.roomName).emit("update_client", room.gameState);
+        }
     }
 
     disconnect() {
